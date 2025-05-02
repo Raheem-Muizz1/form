276f4f6ade553816acebdb00401352fa42163f82
Raheem-Muizz1 <88728237+Raheem-Muizz1@users.noreply.github.com>
2025-05-02 16:37:00 +0100

    Update Form.js

--- Form.js
+++ Form.js
@@ -1,27 +1,4 @@
-document
-  .getElementById("#userForm")
-  .addEventListener("click", async function (event) {
-    event.preventDefault();
-
-    const form = event.target;
-    const data = new FormData(form);
-
-    try {
-      const response = await fetch(form.action, {
-        method: form.method,
-        body: data,
-        headers: {
-          "Accept ": "appliction/json",
-        },
-      });
-      const messageBox = document.getElementById("#message");
-      if (response.ok) {
-        messageBox.innerText = "Thanks for signing up!";
-        form.reset();
-      } else {
-        messageBox.innerText = "Oops! There was a problem.";
-      }
-    } catch (error) {
-      document.getElementById("message").innerText = "Network error";
-    }
-  });
+function lettersOnly(input) {
+  var regex = /[^a-z]/gi;
+  input.value = input.value.replace(regex, "");
+}
