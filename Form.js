15ec0e2326b245666bf63e90108e8497ead190f3
Raheem-Muizz1 <88728237+Raheem-Muizz1@users.noreply.github.com>
2025-05-05 17:27:20 +0100

    Update Form.js

--- Form.js
+++ Form.js
@@ -2,3 +2,24 @@ function lettersOnly(input) {
   var regex = /[^a-z]/gi;
   input.value = input.value.replace(regex, "");
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const userForm = document.getElementById("#user");
+  const dateInput = document.getElementById("#date");
+  const errorMessage = document.getElementById("#error-message");
+
+  userForm.addEventListener("submit", function (event) {
+    const selectedDate = new Date(dateInput.value);
+    const minDate = new Date("1980-01-01");
+    const maxDate = new Date("2015- 12-31");
+
+    if (!dateInput.value) {
+      errorMessage.textContent = "Please select date.";
+      event.preventDefault();
+    } else if (selectedDate < minDate || selectedDate > maxDate) {
+      errorMessage.textContent = "You must be at least 15years";
+      event.preventDefault();
+    } else {
+      errorMessage.textContent = "";
+    }
+  });
+});
